2585e9e335c70d128e81109bb52fd6be7e03a893
user <user@DESKTOP-5SD5AN8>
2026-02-14 11:45:05 +0200

    demonstrating the behavior of the move method

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            vehicles.Add(new Bicecle());
+            vehicles.Add(new Car());
+            vehicles.Add(new Airplane());
+
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Move();
+            }
         }
     }
 }
