518fb1a13f035192ec4c1c5d14c9d0946c9f57a6
user <user@DESKTOP-5SD5AN8>
2026-02-14 11:35:45 +0200

    add abstract class Vechich and child classes

---
+++ Lab6/Task1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    abstract class Vehicle
+    {
+        public string brand;
+        public int speed;
+
+        public abstract void Move();
+    }
+    class Car : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"The {brand} car is moving at {speed} km/h.");
+        }
+    }
+    class Bicecle : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"The {brand} bicycle is moving at {speed} km/h.");
+        }
+    }
+    class Airplane : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"The {brand} airplane is flying at {speed} km/h.");
+        }
+    }
+}
